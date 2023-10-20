6e44fb35a3ac59c0d1f49d08eb75e37ad5406cdd
Adam <schrock-adam@hotmail.com>
2023-10-20 08:46:19 -0400

    components created

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
 import Image from 'next/image';
+import About from './components/About';
+import Contact from './components/Contact';
+import Landing from './components/Landing';
+import Projects from './components/Projects';
 
 export default function Home() {
   return (
-    <main>
-      <div></div>
+    <main className='flex flex-col items-center w-full '>
+      <Landing />
+      <About />
+      <Projects />
+      <Contact />
     </main>
   );
 }
